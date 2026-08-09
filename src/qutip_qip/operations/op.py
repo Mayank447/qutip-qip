36508f1a8b79bd84203aeac169b08f22976fd4aa
Mayank Goel <mayank.goel447@gmail.com>
2026-08-10 01:47:16 +0530

    Minor fixes

--- src/qutip_qip/operations/op.py
+++ src/qutip_qip/operations/op.py
@@ -11,8 +11,8 @@ P = TypeVar("P")
 
 @dataclass(frozen=True, slots=True)
 class Op:
-    uuid: str = field(default_factory=lambda: uuid4().hex)
     _name: str
+    uuid: str = field(default_factory=lambda: uuid4().hex)
     params: tuple[P, ...] = ()
 
     @property
@@ -49,8 +49,11 @@ class OpInstruction:
     style: dict = field(default_factory=dict)  # For circuit draw
 
     def __post_init__(self):
-        if not (isinstance(self.op, Op) or issubclass(self.op, Op)):
-            raise TypeError("op must be a subclass or instance of type Op")
+        # TODO: Enable this after Gate, Measurement class also inherit from Op
+        # if not (isinstance(self.op, Op)) or (
+        #     (isinstance(self.op, type) and issubclass(self.op, Op))
+        # ):
+        #     raise TypeError("op must be a subclass or instance of type Op")
 
-        convert_type_input_to_sequence(self.qreg, "qreg", Int)
-        convert_type_input_to_sequence(self.creg, "creg", Int)
+        convert_type_input_to_sequence(Int, "qreg", self.qreg)
+        convert_type_input_to_sequence(Int, "creg", self.creg)
